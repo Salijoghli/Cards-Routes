0956a6a61bc8da625dbdc859f017fa4e14445432
Salijoghli <guga.kakhadze12@gmail.com>
2023-06-30 16:38:51 +0400

    feat: added loading

--- src/pages/cards/index.tsx
+++ src/pages/cards/index.tsx
@@ -7,32 +7,44 @@ import { CardComponent } from '../../components/card'
 export const CardsPage = () => {
   const navigate = useNavigate()
   const [cards, setCards] = useState<Cards>([])
+  const [isLoading, setIsLoading] = useState(true)
 
   const searchCards = async () => {
-    const response = await fetch('https://fakestoreapi.com/products/')
-    const data = (await response.json()) as Cards
-    setCards(data)
+    setIsLoading(true)
+    try {
+      const response = await fetch('https://fakestoreapi.com/products/')
+      const data = (await response.json()) as Cards
+      setCards(data)
+    } catch (error) {
+      console.error('Error fetching cards:', error)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   useEffect(() => {
     searchCards()
-  }, [])
+  }, [cards])
 
   return (
     <div>
       <h1>Cards</h1>
-      <div className="card-container">
-        {cards.map(card => (
-          <div
-            key={card.id}
-            onClick={() =>
-              navigate(`/card-details/${card.id}`, { state: { card } })
-            }
-          >
-            <CardComponent card={card} />
-          </div>
-        ))}
-      </div>
+      {isLoading ? (
+        <div>loading...</div>
+      ) : (
+        <div className="card-container">
+          {cards.map(card => (
+            <div
+              key={card.id}
+              onClick={() =>
+                navigate(`/card-details/${card.id}`, { state: { card } })
+              }
+            >
+              <CardComponent card={card} />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
