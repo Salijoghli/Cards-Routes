2ff6fd72dc22c294b80450eee9e130de8f4f1b4a
Salijoghli <guga.kakhadze12@gmail.com>
2023-07-04 12:58:08 +0400

    feat: card routes

--- src/pages/cards/index.tsx
+++ src/pages/cards/index.tsx
@@ -7,18 +7,14 @@ import { CardComponent } from '../../components/card'
 export const CardsPage = () => {
   const navigate = useNavigate()
   const [cards, setCards] = useState<Cards>([])
-  const [isLoading, setIsLoading] = useState(true)
 
   const searchCards = async () => {
-    setIsLoading(true)
     try {
       const response = await fetch('https://fakestoreapi.com/products/')
       const data = (await response.json()) as Cards
       setCards(data)
     } catch (error) {
       console.error('Error fetching cards:', error)
-    } finally {
-      setIsLoading(false)
     }
   }
 
@@ -29,22 +25,18 @@ export const CardsPage = () => {
   return (
     <div>
       <h1>Cards</h1>
-      {isLoading ? (
-        <div>loading...</div>
-      ) : (
-        <div className="card-container">
-          {cards.map(card => (
-            <div
-              key={card.id}
-              onClick={() =>
-                navigate(`/card-details/${card.id}`, { state: { card } })
-              }
-            >
-              <CardComponent card={card} />
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="card-container">
+        {cards.map(card => (
+          <div
+            key={card.id}
+            onClick={() =>
+              navigate(`/card-details/${card.id}`, { state: { card } })
+            }
+          >
+            <CardComponent card={card} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
